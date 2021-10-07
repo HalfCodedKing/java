8eac470e3be94437d9f4fc5be194fc2f2569836d
HalfCodedKing <hardikkakkar25@gmail.com>
2021-10-07 18:46:57 +0530

    first commit

---
+++ literal.java
@@ -0,0 +1,14 @@
+/**
+ * literal
+ */
+public class literal {
+
+    public static void main(String[] args) {
+        byte age = 34 ;
+        char ch = 'A';
+        float f = 5.6f;
+        System.out.println(age);
+
+    }
+    
+}
